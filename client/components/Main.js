eb08e9d010cda891c80f4b030a37d503cbc49e44
Andrew Yeonsoo Han <drewhan90@gmail.com>
2017-02-03 22:32:16 -0800

    increment counter

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 class Main extends React.Component {
   constructor(){
     super();
     this.state = {
+        count: 0,
+        speed: 3000,
+        messages: ['Hey there!', "My name is Andrew Yeonsoo Han", 'You can just call me Andrew', 'My am a passionate web developer', 'Who takes UX/UI Design seriously!']
     };
+  }
+  _increment(){
+    this.setState({
+      count: this.state.count + 1
+    })
+    if(this.state.count >= this.state.messages.length){
+      this.setState({ count: 0 })
+    }
+  }
+  componentDidMount(){
+    setInterval(this._increment.bind(this), this.state.speed);
   }
   render(){
     return (
       <div id="mainContainer">
         <Nav />
         <div id="mainMsg">
-          <h1></h1>
+          <h1>{this.state.messages[this.state.count]}</h1>
         </div>
       </div>
     )
   }
 }
 
-const Nav = (props) => 
+const Nav = () => 
   <nav id="mainNav">
     <ul>
       <li><button className="aboutBtn navBtn"></button></li>
